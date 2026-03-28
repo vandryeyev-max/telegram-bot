68186847d85d263baf84f751daf66db5285bd3e1
vandryeyev-max <vandryeyev@odessa-gymnasium8.ukr.education>
2026-03-28 15:45:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,21 @@ import os
 import telebot
 from flask import Flask, send_file
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import threading
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 app = Flask(__name__)
 
-# --- WEB (чтобы Railway не выключал) ---
 @app.route("/")
 def home():
     return "Bot is running"
-
-@app.route("/ping")
-def ping():
-    return {"status": "ok"}
 
 @app.route("/app")
 def web_app():
     return send_file("index.html")
 
-
-# --- TELEGRAM BOT ---
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = InlineKeyboardMarkup()
@@ -36,13 +30,9 @@ def start(message):
 
     bot.send_message(message.chat.id, "Жми кнопку 👇", reply_markup=markup)
 
-
 def run_bot():
     bot.infinity_polling()
 
-
-# --- ЗАПУСК ---
 if __name__ == "__main__":
-    import threading
     threading.Thread(target=run_bot).start()
     app.run(host="0.0.0.0", port=10000)
