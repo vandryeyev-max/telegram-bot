4ca7cee0c3fb079f554f73f931f7faa73c7b2651
vandryeyev-max <vandryeyev@odessa-gymnasium8.ukr.education>
2026-03-28 15:15:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 from flask import Flask
-
+from flask import send_file
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
@@ -14,11 +14,23 @@ def home():
 @app.route("/ping")
 def ping():
     return {"status": "ok"}
+@app.route("/app")
+def web_app():
+    return send_file("index.html")
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот работает 🚀")
+    markup = InlineKeyboardMarkup()
 
+    btn = InlineKeyboardButton(
+        text="🎮 Открыть казино",
+        web_app={"url": "https://telegram-bot-production-b86b.up.railway.app/app"}
+    )
+
+    markup.add(btn)
+
+    bot.send_message(message.chat.id, "Жми кнопку 👇", reply_markup=markup)
 def run_bot():
     bot.infinity_polling()
 
