5813b58bb415f586ac0003f23a9bb56b7be0532b
vandryeyev-max <vandryeyev@odessa-gymnasium8.ukr.education>
2026-03-29 11:48:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 import telebot
 import os
-from flask import Flask, render_template
+import threading
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from flask import Flask
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 # --- САЙТ ---
 @app.route("/")
 def index():
     return "SITE WORKING"
 
-# --- БОТ (ПОКА НЕ ЗАПУСКАЕМ) ---
+# --- БОТ ---
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот работает")
+    markup = InlineKeyboardMarkup()
 
-# --- ЗАПУСК ---
+    web_app = WebAppInfo("https://production-b86b.up.railway.app")
+
+    button = InlineKeyboardButton(
+        text="🎰 Играть",
+        web_app=web_app
+    )
+
+    markup.add(button)
+
+    bot.send_message(
+        message.chat.id,
+        "Жми кнопку 👇",
+        reply_markup=markup
+    )
+
+def run_bot():
+    bot.infinity_polling()
+
+# --- СТАРТ ---
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    print("STARTING WEB SERVER ON PORT", port)
+    threading.Thread(target=run_bot).start()
 
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
